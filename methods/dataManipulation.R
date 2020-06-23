d5d346c30ddfd9f78f446c0519dbbfe7443dba4c
Xinrui-Wu <55121885+Xinrui-Wu@users.noreply.github.com>
2020-06-23 07:26:53 -0400

    Add files via upload

--- methods/dataManipulation.R
+++ methods/dataManipulation.R
@@ -1,7 +1,7 @@
 ## BP_EMA data manipulation function
 ## Script by : Xinrui Wu
 
-# Main function: bp_ema
+# Main function: BPEMA
 # Assist function: press_count
 
 
@@ -74,11 +74,13 @@ press_count = function(bp, ema, id, k){
 }
 
 
-bp_ema = function(BP, EMA, time.win){
+BPEMA = function(BP, EMA, time.win, returnPlot = 1){
   # Input:
   #   BP: Button Press data, including variables "ID, timestamp, time_day, run.time"
   #   EMA: EMA data, including variables "ID, timestamp, date, time_day, run.time, sitb_si_sum"
   #   time.win: a vector of time windows for finding presses around a survey
+  #   returnPlot: a logic variable for showing the visualization plot or not. 
+  #               Defult is 1(show the plot)
   # Output:
   #   bp_ema: dataframe including variables "ID, timestamp, date, time_day, si_score" 
   #           and variables "pre_press, post_press" for different time windows.
@@ -112,6 +114,66 @@ bp_ema = function(BP, EMA, time.win){
     }
   }
   
+  if(returnPlot == 1){
+    df_plot = count[,2:3]
+    df_plot = as.matrix(filter(df_plot, rowSums(df_plot) != 0))
+    dim = dim(df_plot)[1]
+    
+    if(n_win > 1){
+      for (k in 2:n_win){
+        temp = count[, c(2*k, 2*k+1)]
+        temp = as.matrix(filter(temp, rowSums(temp) != 0))
+        df_plot = rbind(df_plot, temp)
+        dim = c(dim, dim(temp)[1])
+      }
+    }
+    
+    df_plot = data.frame(Window = as.factor(rep(time.win, times = 2*dim)), 
+                         Time = rep(rep(c("pre", "post"), n_win), times = rep(dim, each = 2)), 
+                         Count = as.numeric(df_plot))
+    require(ggplot2)
+    plot = ggplot(df_plot, aes(x=Window, y=Count, fill=Time)) + 
+      geom_boxplot() +
+      labs(title = "Count of presses for observations with non-zero press in the window")
+    print(plot)
+  }
+  
   bp_ema = as.data.frame(cbind(ema, count[,-1]))
   return(bp_ema)
 }
+
+plot_BPEMA = function(bp_ema, time.win){
+  # Input:
+  #   bp_ema: the Output from BPEMA function
+  #   time.win: a vector of time windows for finding presses around a survey
+  #             (the same with the input of BPEMA)
+  
+  # Output:
+  #   Visualization plot for bp_ema
+  
+  count = bp_ema[, -(1:6)]
+  n_win = dim(count)[2]/2
+  
+  df_plot = count[,1:2]
+  df_plot = as.matrix(filter(df_plot, rowSums(df_plot) != 0))
+  dim = dim(df_plot)[1]
+  
+  if(n_win > 1){
+    for (k in 2:n_win){
+      temp = count[, c(2*k-1, 2*k)]
+      temp = as.matrix(filter(temp, rowSums(temp) != 0))
+      df_plot = rbind(df_plot, temp)
+      dim = c(dim, dim(temp)[1])
+    }
+  }
+  
+  df_plot = data.frame(Window = as.factor(rep(time.win, times = 2*dim)), 
+                       Time = rep(rep(c("pre", "post"), n_win), times = rep(dim, each = 2)), 
+                       Count = as.numeric(df_plot))
+  require(ggplot2)
+  print(ggplot(df_plot, aes(x=Window, y=Count, fill=Time)) + 
+    geom_boxplot() +
+    labs(title = "Count of presses for observations with non-zero press in the window"))
+}
+
+
